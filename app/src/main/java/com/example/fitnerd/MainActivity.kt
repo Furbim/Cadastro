7de5ff9c336d0e629e4f00e1f0154325e42730f3
Thiago <thiagofreitasofc202@gmail.com>
2025-03-25 09:48:38 -0300

    Iniciando o back-end trabalhando com binding

--- app/src/main/java/com/example/fitnerd/MainActivity.kt
+++ app/src/main/java/com/example/fitnerd/MainActivity.kt
@@ -1,47 +1,53 @@
 package com.example.fitnerd
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fitnerd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val imgPerfil = findViewById<ImageView>(R.id.img_user);
-        val edtNome = findViewById<EditText>(R.id.edt_nome);
-        val edtEmail = findViewById<EditText>(R.id.edt_email);
-        val edtIdade = findViewById<EditText>(R.id.edt_idade);
-        val edtSenha = findViewById<EditText>(R.id.edt_senha);
-        val btnCadastro = findViewById<Button>(R.id.btn_cadastro);
-        val btnFoto = findViewById<Button>(R.id.btn_enviarFoto);
-
-        var nome = edtNome.text.toString();
-        var email = edtEmail.text.toString();
-        var idade = edtIdade.text.toString().toInt()
-        var senha = edtSenha.text.toString();
 
 
+
+        binding.btnCadastro.setOnClickListener{
+            exibirInformacoes();
+        }
 
 
 
     }
 
+    fun exibirInformacoes(){
 
+        val nome = binding.edtNome.text.toString();
+        Log.d("INFO_CLIENT","NOME: ${nome}");
 
+        val email = binding.edtEmail.text.toString();
+        Log.d("INFO_CLIENT","EMAIL: ${email}");
 
+        val idade = binding.edtIdade.text.toString().toInt();
+        Log.d("INFO_CLIENT","IDADE: ${idade}");
 
-
+        val senha = binding.edtSenha.text.toString();
+        Log.d("INFO_CLIENT","SENHA: ${senha}");
+    }
 }
